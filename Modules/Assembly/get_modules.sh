4ab0ac7442f35b33d65a860dcacfb0b47e9ade0d
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2025-06-19 11:53:03 +0100

    Bump/assembler v0.7 (#6)
    * Delete start.sh
    
    * Delete stop.sh
    
    * Update get_service_modules.sh
    
    Bumped assembler to v0.7.0
    Added advice.detachedHead false before downloading
    
    * Rename get_service_modules.sh to get_modules.sh
    
    * Renamed UserConfig to Config
    
    * Renamed ServiceModules to Modules
    
    * Remove User and Service from README
    
    * Made get_modules.sh executable

--- Modules/Assembly/get_modules.sh
+++ Modules/Assembly/get_modules.sh
@@ -3,13 +3,14 @@
 # Minimal launcher for the Shoestring Assembler
 
 # What version of the Assembler to use
-VERSION="v0.4.0"
+VERSION="v0.7.0"
 
 # Get location of this script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Download Shoestring Assembler
 echo "Downloading Shoestring Assembler" $VERSION
+git config --global advice.detachedHead false
 git clone --quiet https://github.com/DigitalShoestringSolutions/ShoestringAssembler -b $VERSION $SCRIPT_DIR/ShoestringAssembler
 
 # Run Shotestring Assembler
